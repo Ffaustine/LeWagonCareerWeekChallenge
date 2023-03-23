218f89b962d9c6cd277cfd8f2384c3a4d46d99de
Faustine Fourcade <faustine.fourcade@hotmail.fr>
2023-03-23 16:48:36 +0100

    challenge en cours : j'arrive à sléectionner les postcodes et le nom des musées

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -1,15 +1,25 @@
-class MuseumsController < ApplicationController
-  def index
-    @museums
-  end
-end
-
-
 require "json"
 require "open-uri"
 
-url = "https://api.mapbox.com/tokens/v2/ffourcade?access_token=MAPBOX_ACCESS_TOKEN/"
-user_serialized = URI.open(url).read
-user = JSON.parse(user_serialized)
+class MuseumsController < ApplicationController
 
-puts "#{user["name"]} - #{user["bio"]}"
+  def index
+    lat = params[:lat]
+    lng = params[:lng]
+    url = "https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?proximity=#{lng},#{lat}&access_token=#{ENV['MAPBOX_ACCESS_TOKEN']}"
+    response = URI.open(url).read
+    data = JSON.parse(response)
+
+    museums = []
+
+    data["features"].map do |feature|
+      postcode = feature["context"][0]["text"]
+      museums << feature["text"]
+    end
+
+    group_museums = museums.group_by(postcode)
+
+
+    render json: group_museums
+  end
+end
